dc5d7c2a98f3a0f3953570607a35a259def490f3
thomasmudge <alephcult@gmail.com>
2019-09-24 18:13:24 -0500

    Minor dancefloor.html changes

--- src/REDACTEDDancer.js
+++ src/REDACTEDDancer.js
@@ -1,8 +1,6 @@
 const makeREDACTEDDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
 };
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
 
 //import makeDancer superclass methods
 makeREDACTEDDancer.prototype = Object.create(makeDancer.prototype);
