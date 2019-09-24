f5f892e3069496c08256c9b0354b21c26011feae
thomasmudge <alephcult@gmail.com>
2019-09-24 15:47:15 -0500

    Set background image and created outlines for 2 additional dancers

---
+++ src/REDACTEDDancer.js
@@ -0,0 +1,20 @@
+const makeREDACTEDDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+};
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+//import makeDancer superclass methods
+makeREDACTEDDancer.prototype = Object.create(makeDancer.prototype);
+//reset constructor
+makeREDACTEDDancer.prototype.constructor = makeREDACTEDDancer;
+
+makeREDACTEDDancer.prototype.step = function(timeBetweenSteps) {
+    makeDancer.prototype.step.call(this, timeBetweenSteps);
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+  };
+
+window.makeREDACTEDDancer = makeREDACTEDDancer;
---
+++ src/XKeyscoreDancer.js
@@ -0,0 +1,18 @@
+const makeXKeyscoreDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+};
+
+//import makeDancer superclass methods
+makeXKeyscoreDancer.prototype = Object.create(makeDancer.prototype);
+//reset constructor
+makeXKeyscoreDancer.prototype.constructor = makeBlinkyDancer;
+
+makeXKeyscoreDancer.prototype.step = function(timeBetweenSteps) {
+    makeDancer.prototype.step.call(this, timeBetweenSteps);
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+  };
+
+window.makeXKeyscoreDancer = makeXKeyscoreDancer;
